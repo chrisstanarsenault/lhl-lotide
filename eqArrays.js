a4c029c98d952a8e8cdf03588e0b02488371ae7d
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-08-21 19:51:20 -0400

    added function for comparing two simple arrays

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arrA, arrB) => {
+  if (arrA.length !== arrB.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrA.length; i++) {
+      if (arrA[i] !== arrB[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
