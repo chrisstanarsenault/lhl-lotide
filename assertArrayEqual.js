6f2bf814788c1f69667caa922981ee5a72013fd0
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-09-07 02:16:27 -0400

    added test file and module for assertArrayEquals.js

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = (arrA, arrB) => {
-  if (arrA.length !== arrB.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arrA.length; i++) {
-      if (arrA[i] !== arrB[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require("./eqArrays");
 
 const assertArrayEqual = (arrA, arrB) => {
   if (eqArrays(arrA, arrB)) {
@@ -19,5 +8,4 @@ const assertArrayEqual = (arrA, arrB) => {
   }
 };
 
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
-assertArrayEqual([1, 2, 3], [1, 3, 3]);
+module.exports = assertArrayEqual;
---
+++ test/assertArrayEqualsTest.js
@@ -0,0 +1,5 @@
+// const eqArrays = require("../eqArrays");
+const assertArrayEqual = require("../assertArrayEqual");
+
+assertArrayEqual([1, 2, 3], [1, 2, 3]);
+assertArrayEqual([1, 2, 3], [1, 3, 3]);
