7c97b3dd07758e0f0226c66e09477fa596a94953
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-09-07 02:20:14 -0400

    added test file and module for middle.js

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = (arrA, arrB) => {
-  if (arrA.length !== arrB.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arrA.length; i++) {
-      if (arrA[i] !== arrB[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArrayEqual = (arrA, arrB) => {
-  if (eqArrays(arrA, arrB)) {
-    console.log(`✅✅✅ Assertion Passed: ${arrA} === ${arrB}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arrA} === ${arrB}`);
-  }
-};
-
 const middle = (arr) => {
   if (arr.length % 2 !== 0) {
     return [arr[Math.ceil(arr.length * 0.5 - 1)]];
@@ -30,7 +9,4 @@ const middle = (arr) => {
   }
 };
 
-assertArrayEqual(middle([1, 2, 3]), [2]);
-assertArrayEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArrayEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
